6de2ba142ade89c15b8617cc3ceeab5fc8c83441
malilless <omalych@kse.org.ua>
2025-05-31 21:45:38 +0300

    Second homework

--- HelloWorld.py
+++ HelloWorld.py
@@ -42,8 +42,10 @@ for i in range(2,11,2):
 
 print("Завдання 8")
 n = int(input("Введіть число n: "))
-for i in range(1,n,1):
-    n += 1
+sum = 0
+for i in range(1,n+1):
+    sum += i
+print(sum)
 
 print("Завдання 9")
 for i in range(10,0,-1):
